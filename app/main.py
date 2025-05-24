eb29584de4987f52c5fac3b56b3a6f2ba1f67952
andre <andre.baron@online.de>
2025-05-24 19:05:25 +0200

    add versioning route

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import logging
 app = FastAPI()
 
 
-
+putzplanVersion="0.7.dev"
 
 @app.on_event("startup")
 def on_startup():
@@ -18,6 +18,11 @@ app.include_router(users.router, prefix="/api/users", tags=["users"])
 app.include_router(tasks.router, prefix="/api/tasks", tags=["tasks"])
 app.include_router(logs.router, prefix="/api/logs", tags=["logs"])
 app.include_router(backup.router, prefix="/api/backup", tags=["backup"])
+
+
+@app.get("/api/putzplanVersion")
+def getPutzplanVersion():
+    return {"message": putzplanVersion}
 
 @app.get("/api/ping")
 def ping():
